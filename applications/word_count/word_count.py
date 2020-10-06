ae11b574b85ae07ed3deb23c3dc6398b74f7f91c
dallasjames <dallasjames42@gmail.com>
2020-10-05 19:42:07 -0600

    word_count done

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,21 @@
+import string
+
+
 def word_count(s):
-    # Your code here
+    s = s.replace("\r", " ")
+    s = s.replace("\n", " ")
+    s = s.replace("\t", " ")
+    words_with_punc = s.lower().split(" ")
+    count = {}
+    if len(s) > 0:
+        for i in words_with_punc:
+            words = i.strip(string.punctuation)
+            if len(words) > 0:
+                if words in count:
+                    count[words] += 1
+                else:
+                    count[words] = 1
+    return count
 
 
 
