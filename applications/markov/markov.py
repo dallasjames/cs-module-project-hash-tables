a7e5ac64c69b9427c04e2e9b272b442646ec00ad
dallasjames <dallasjames42@gmail.com>
2020-10-08 17:10:40 -0600

    markov done

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -5,9 +5,42 @@ with open("input.txt") as f:
     words = f.read()
 
 # TODO: analyze which words can follow other words
-# Your code here
+test = "Banana 1 three banana 1 1 banana 2 2 three banana. \"Start stop.\""
+words = words.strip("\n")
+words = words.split(" ")
+
+word_dict = {}
+
+for index, word in enumerate(words):
+    if word in word_dict:
+        if index + 1 < len(words):
+            word_dict[word].append(words[index + 1])
+        else:
+            word_dict[word] = None
+    else:
+        if index + 1 < len(words):
+            word_dict[word] = [words[index + 1]]
+        else:
+            word_dict[word] = None
 
 
 # TODO: construct 5 random sentences
-# Your code here
+def markov_sentence(word_dict):
+    punctuation = ".?!"
+    starters = []
+    stoppers = []
+    for word in word_dict:
+        if word[0].isupper() or (word[0] == "\"" and word[1].isupper()):
+            starters.append(word)
+        if word[-1] in punctuation or (word[-1] == "\"" and word[-2] in punctuation):
+            stoppers.append(word)
+    string = ""
+    word = random.choice(starters)
+    while word not in stoppers:
+        string += word + " "
+        word = random.choice(word_dict[word])
+    string += word + " "
+    return string
 
+
+print(markov_sentence(word_dict))
