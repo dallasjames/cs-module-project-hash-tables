b2d78da67c4ee67edbe6686757f6e69a98ff2d58
dallasjames <dallasjames42@gmail.com>
2020-10-08 17:47:50 -0600

    histo done

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,28 @@
-# Your code here
+import string
 
+
+with open("robin.txt") as f:
+    page = f.read()
+
+
+def histo(words):
+    hgram = {}
+    words = words.translate(str.maketrans('', '', string.punctuation))
+    words = words.lower()
+    words = words.replace("\r", " ")
+    words = words.replace("\n", " ")
+    words = words.replace("\t", " ")
+    words = words.split(" ")
+    for i in words:
+        if i in hgram:
+            hgram[i] += "#"
+        else:
+            hgram[i] = "#"
+
+    dictionary_items = hgram.items()
+    sorted_items = sorted(dictionary_items)
+    for key, value in sorted_items:
+        print(key, ' : ', value)
+
+
+histo(page)
